09a105105a4cad401a18ef24dfa452cda05f817f
Deepak Patra <deepak@itobuz.com>
2023-03-03 18:43:46 +0530

    module v1

---
+++ modules/index.mjs
@@ -0,0 +1,12 @@
+// import * as math from './math.mjs';
+
+import  add from './math.mjs';
+
+
+import  {sub} from './math.mjs';
+// const math = require("./math");
+
+console.log(add(1,3));
+console.log(sub(1,3));
+// console.log(math.add(1,3));
+// console.log(math.sub(1,3));
