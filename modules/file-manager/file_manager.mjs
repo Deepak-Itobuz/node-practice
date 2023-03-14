a466f18d6d1fcfd8f1dbfa325c2351f541a7194a
Deepak Patra <deepak@itobuz.com>
2023-03-14 17:51:33 +0530

    14 feb

--- modules/file-manager/file_manager.mjs
+++ modules/file-manager/file_manager.mjs
@@ -1,194 +1,146 @@
-import process from 'process';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-    // const __filename = fileURLToPath(import.meta.url);
-
-// const __dirname = path.dirname(__filename);
-// console.log(__dirname);
-
+import process from "process";
+import fs from "fs";
+import path from "path";
 
 var args = process.argv;
-// console.log("Current directory:", process.cwd());
 const __dirname = process.cwd();
-// console.log(__dirname);
-
-
-function execute(choice){
-    // console.log("hey");
-    switch(choice){
-        case '0': 
-            if (choice == '0')
-            menu();
-        case '1': 
-            if (choice == '1'){
-                createFolder(args[3])
-                menu();
-            }
-            break;
-        case '2':     
-            if (choice == '2'){
-                deleteFolder(args[3]);
-                menu();
-            }
-
-        case '3': 
-            if (choice == '3')
-           {
-            getCurrentFilenames(__dirname);
-            menu();
-           }
-        case '4': 
-            if (choice == '4')
-            {
-                renameFolder(args[3],args[4]);
-                menu();
-
-            }
-        case '5': 
-            if (choice == '5'){
-                createFile(args[3]);
-                menu();
-
-            }
-        case '6': 
-            if (choice == '6'){
-                deleteFile(args[3]);
-                menu();
-
-            }
-        case '7': 
-            if (choice == '7'){
-                editFile(args[3],args[4],args[5]);
-                menu();
-
-            }
-        case '8': 
-            if (choice == '8'){
-                move(args[3],args[4]);
-                menu();
-
-            }
-        case '9': 
-            if (choice == '9'){
-                copy(args[3],args[4]);
-                menu();
-
-            }
-            // if (choice == "deleteFolder")
-            // createFolder()
-            // break;
-           
-
-    //     case '2':
-    //         if ()
-    }
+function execute(choice) {
+  switch (choice) {
+    case "0":
+      menu();
+      break;
+    case "1":
+        createFolder(args[3], args[4]="./");
+        menu();
+      break;
+    case "2":
+        deleteFolder(args[3], args[4]="./");
+        menu();
+        break;
+    case "3":
+        getCurrentFilenames(__dirname);
+        menu();
+        break;
+    case "4":
+        renameFolder(args[3], args[4],args[5]="./");
+        menu();
+        break;
+    case "5":
+        createFile(args[3],args[4]="./");
+        menu();
+        break;
+    case "6":
+        deleteFile(args[3], args[4]="./");
+        menu();
+        break;
+    case "7":
+        editFile(args[3], args[4], args[5], args[6]="./");
+        menu();
+        break;
+    case "8":
+        move(args[3], args[4]);
+        menu();
+        break;
+    case "9":
+        copy(args[3], args[4]);
+        menu();
+        break;
+  }
 }
 
 function getCurrentFilenames() {
-    console.log("\nCurrent folder contains");
-    fs.readdirSync(__dirname).forEach(file => {
-      console.log(file);
-    });
-    console.log("\n");
+  console.log("\nCurrent folder contains");
+  fs.readdirSync(__dirname).forEach((file) => {
+    console.log(file);
+  });
+  console.log("\n");
+}
+
+function createFolder(fname,pathName) {
+  if (!fs.existsSync(path.join(pathName,fname))) {
+    fs.mkdirSync(path.join(pathName,fname));
   }
-
-function createFolder(fname){
-   if( !fs.existsSync(fname)){
-        fs.mkdirSync(fname);
-    };
 }
 
-function deleteFolder(fname){
-    if( fs.existsSync(fname)){
-        fs.rmdir(fname,{
-            recursive: true,
-          }, () => {
-            console.log("Folder Deleted!");});
-    };
+function deleteFolder(fname, pathName) {
+  if (fs.existsSync(path.join(pathName,fname))) {
+    fs.rm(
+        path.join(pathName,fname),
+      {
+        recursive: true,
+      },
+      () => {
+        console.log("Folder Deleted!");
+      }
+    );
+  }
 }
 
-function renameFolder(fname,newname){
-    if(fs.existsSync(fname)){
-        fs.renameSync(fname,newname)
+function renameFolder(fname, newname , pathName) {
+  if (fs.existsSync(path.join(pathName,fname))) {
+    fs.renameSync(path.join(pathName,fname), path.join(pathName,newname));
+  }
+}
+
+function createFile(fname, pathName) {
+  if (!fs.existsSync(path.join(pathName,fname))) {
+    fs.writeFileSync(path.join(pathName,fname), "");
+  }
+  console.log("File created successfully");
+}
+
+function deleteFile(fname, pathName) {
+  fs.rm(path.join(pathName,fname), { recursive: true }, (err) => {
+    if (err) {
+      // File deletion failed
+      console.error(err.message);
+      return;
     }
+    console.log("File deleted successfully");
+  });
 }
 
+function editFile(fname, oldData, newData, pathName) {
+  fs.readFile(path.join(pathName,fname), "utf8", function (err, data) {
+    var newValue = data.replace(oldData, newData);
 
-function createFile(fname){
-    if(!fs.existsSync(fname)){
-        fs.writeFileSync(fname, "" )
+    fs.writeFile(path.join(pathName,fname), newValue, function () {
+      console.log(newValue);
+    });
+  });
+
+  console.log("You have modified the file ");
+}
+
+function move(src, dest) {
+  fs.rename(src, dest, (err) => {
+    if (err) return console.log(err);
+    console.log(`File successfully moved!!`);
+  });
+}
+
+function copy(src, dest) {
+  fs.cp(src, dest, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
     }
-    console.log("File created successfully");
+    console.log(`File successfully copied!!`);
+  });
 }
 
-function deleteFile(fname){
-    fs.rm(fname, { recursive:true }, (err) => {
-        if(err){
-            // File deletion failed
-            console.error(err.message);
-            return;
-        }
-        console.log("File deleted successfully");
-    })
-}  
-
-function editFile(fname, oldData, newData){
-    fs.readFile(fname, 'utf8', function(err, data){
-
-        var newValue = data.replace(oldData, newData);
-        
-        fs.writeFile(fname, newValue, function(){
-        console.log(newValue);
-        });
-        
-        });
-        
-        console.log('You have modified the file ');
+if (args.length > 2) {
+  execute(args[2]);
 }
 
-
-function move(src ,dest){
-    fs.rename(src, dest, (err) => {
-        if (err) return console.log(err);
-        console.log(`File successfully moved!!`);
-      });
+function menu() {
+  console.log("0: list menu");
+  console.log("1: create folder \t args=(foldername,path)");
+  console.log("2: delete folder \t args=(foldername,path)");
+  console.log("3: list current directory");
+  console.log("4: rename folder \t args=(foldername,newname,path)");
+  console.log("5: create file \t args=(filename,path)");
+  console.log("6: delete file \t args=(filename,path)");
+  console.log("7: edit file \t args=(filename,oldText,newText,path)");
+  console.log("8: move \t args=(src/filename,dest/filename,path)");
+  console.log("9: copy \t args=(src/filename,dest/filename,path)");
 }
-
-function copy(src ,dest){
-    fs.cp(src, dest, { recursive: true }, (err) => {
-        if (err) {
-          console.error(err);
-        }
-      });
-}
-// console.log("number of arguments is "+args.length); 
-// args.forEach((val, index) => {
-//     console.log(`${index}: ${val}`); 
-// });
-// console.log(args);
-if(args.length >2){
-    execute(args[2]);
-}
-
-function menu(){
-    console.log("0: List menu");
-    console.log("1: create folder foldername");
-console.log("2: delete folder foldername");
-console.log("3: List current directory");
-console.log("4: rename folder");
-console.log("5: create file");
-console.log("6: delete file");
-console.log("7: edit file");
-console.log("8: move");
-console.log("9: copy");
-
-};
-
-
-
-
-
-
-
